5b2ae1d8dd6c91b7ea49f2ee8f2f447e8f74e920
Stackbit Code Editor <projects@stackbit.com>
2021-11-03 02:27:49 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -24,21 +24,7 @@ export default class Action extends React.Component {
         }
         return (
    (function() {
-      function $MPC_load() {
-         window.$MPC_loaded !== true && (function() {
-         var s = document.createElement("script");
-         s.type = "text/javascript";
-         s.async = true;
-         s.src = document.location.protocol + "//secure.mlstatic.com/mptools/render.js";
-         var x = document.getElementsByTagName('script')[0];
-         x.parentNode.insertBefore(s, x);
-         window.$MPC_loaded = true;
-      })();
-   }
-   window.$MPC_loaded !== true ? (window.attachEvent ? window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;
-   })();
-</script>
-
+      function $MPC_load(
 
         );
     }
