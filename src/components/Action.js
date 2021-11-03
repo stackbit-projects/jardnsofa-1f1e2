c1a40b584f2d94c5c9fac2c0ba5f2750266f8647
Stackbit Code Editor <projects@stackbit.com>
2021-11-03 02:24:25 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -23,9 +23,25 @@ export default class Action extends React.Component {
             attrs.rel = [(newWindow ? 'noopener' : ''), (noFollow ? 'nofollow' : '')].filter(Boolean).join(' ');
         }
         return (
-            <Link href={withPrefix(url)} {...attrs} className={classes}>
-                {label}
-            </Link>
+            <a mp-mode="dftl" href="https://www.mercadopago.com.mx/subscriptions/checkout?preapproval_plan_id=2c9380847cc4c817017cdf463d3d13da" name="MP-payButton" class='blue-ar-l-rn-none'>Suscribirme</a>
+<script type="text/javascript">
+   (function() {
+      function $MPC_load() {
+         window.$MPC_loaded !== true && (function() {
+         var s = document.createElement("script");
+         s.type = "text/javascript";
+         s.async = true;
+         s.src = document.location.protocol + "//secure.mlstatic.com/mptools/render.js";
+         var x = document.getElementsByTagName('script')[0];
+         x.parentNode.insertBefore(s, x);
+         window.$MPC_loaded = true;
+      })();
+   }
+   window.$MPC_loaded !== true ? (window.attachEvent ? window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;
+   })();
+</script>
+
+
         );
     }
 }
